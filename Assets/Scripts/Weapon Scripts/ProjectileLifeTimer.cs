176a7630f1a64e89236fa19e59a3ea2a28b27fd6
dz1mka <dzimka@gmail.com>
2025-11-14 13:16:06 +0200

    76 - Removing Projectiles From The Game

---
+++ Assets/Scripts/Weapon Scripts/ProjectileLifeTimer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ProjectileLifeTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float timer = 3f;
+
+    private void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+}
