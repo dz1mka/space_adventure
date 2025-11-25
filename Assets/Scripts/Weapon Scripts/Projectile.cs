3592e6471eb22a7bb808eb29d67bb55e379e62e9
dz1mka <dzimka@gmail.com>
2025-11-25 17:10:17 +0200

    86 - Projectile Collision Detection

--- Assets/Scripts/Weapon Scripts/Projectile.cs
+++ Assets/Scripts/Weapon Scripts/Projectile.cs
@@ -39,4 +39,22 @@ public class Projectile : MonoBehaviour
     {
         transform.Translate(0f, speed * Time.deltaTime, 0f);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(TagManager.PLAYER_TAG))
+        {
+            Debug.Log("Projectile hit Player");
+        }
+
+        if (collision.CompareTag(TagManager.ENEMY_TAG) || collision.CompareTag(TagManager.METEOR_TAG))
+        {
+            Debug.Log("Projectile hit Enemy");
+        }
+
+        if (!collision.CompareTag(TagManager.UNTAGGED_TAG) && !collision.CompareTag(TagManager.COLLECTABLE_TAG)) 
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
