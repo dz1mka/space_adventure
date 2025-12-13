8edd052b22fa3cf0d8fd0d080de88ebcdfcd96c2
dz1mka <dzimka@gmail.com>
2025-12-13 14:05:57 +0200

    Revert "97 - Collectable Script"
    This reverts commit b3aad77a97b0bb6d14270b84bc0883a22f76c791.

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -13,8 +13,6 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField]
     private GameObject playerDamageFX;
 
-    private Collectable collectable;
-
     private void Awake()
     {
         playerHealth = playerMaxHealth;
@@ -39,22 +37,6 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (collision.CompareTag(TagManager.COLLECTABLE_TAG))
-        {
-            collectable = collision.GetComponent<Collectable>();
-            if (collectable.type == CollectableType.Health)
-            {
-                playerHealth += collectable.healthValue;
-                if (playerHealth > playerMaxHealth)
-                {
-                    playerHealth = playerMaxHealth;
-                }
-
-                Destroy(collectable.gameObject);
-            }
-        }
-
         if (collision.CompareTag(TagManager.METEOR_TAG))
         {
             TakeDamage(Random.Range(20, 40));
